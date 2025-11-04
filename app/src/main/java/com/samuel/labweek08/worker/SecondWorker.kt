e1c1b9946b76d8f9b605c6f9417259adb36a46e4
MBotakk <samuel.itachis4@gmail.com>
2025-11-05 05:58:37 +0700

    Commit No. 1 – add first & second worker

---
+++ app/src/main/java/com/samuel/labweek08/worker/SecondWorker.kt
@@ -0,0 +1,18 @@
+package com.samuel.labweek08.worker
+
+import android.content.Context
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class SecondWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+    override fun doWork(): Result {
+        val id = inputData.getString(FirstWorker.INPUT_DATA_ID)
+        Thread.sleep(3000L)
+        val outputData = Data.Builder()
+            .putString(FirstWorker.OUTPUT_DATA_ID, id)
+            .build()
+        return Result.success(outputData)
+    }
+}
